b09721efc4fc58c92ddf112cd9d94e6c5869a4c7
Scott Kerkvliet <scott_kerkvliet@hotmail.com>
2019-12-04 20:37:29 -0500

    Day 4 solutions

---
+++ Day 4/problem_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace problem_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(CountPasswords(138241, 674034));
+    }
+    
+    static int CountPasswords(int min, int max) {
+      var totalPasswords = 0;
+
+      for (int num = min; num <= max; num++) {
+        var digits = new List<int>(num.ToString().ToCharArray().Select((c) => int.Parse(c.ToString())));
+        var prevDigit = -1;
+        var hasDouble = false;
+        var isIncreasing = true;
+
+        foreach (var digit in digits) {
+          if (digit < prevDigit) {
+            isIncreasing = false;
+            break;
+          }
+          else if (digit == prevDigit) {
+            hasDouble = true;
+          }
+          prevDigit = digit;
+        }
+
+        totalPasswords += (hasDouble && isIncreasing ? 1 : 0);
+      }
+
+      return totalPasswords;
+    }
+  }
+}
---
+++ Day 4/problem_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace problem_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(CountPasswords(138241, 674034));
+    }
+    
+    static int CountPasswords(int min, int max) {
+      var totalPasswords = 0;
+
+      for (int num = min; num <= max; num++) {
+        var digits = new List<int>(num.ToString().ToCharArray().Select((c) => int.Parse(c.ToString())));
+        var prevDigit = -1;
+        var hasDouble = false;
+        var currentStreak = 1;
+        var isIncreasing = true;
+
+        foreach (var digit in digits) {
+          if (digit < prevDigit) {
+            isIncreasing = false;
+            break;
+          }
+          else if (digit == prevDigit) {
+            currentStreak++;
+          }
+          else {
+            hasDouble = hasDouble || currentStreak == 2;
+            currentStreak = 1;
+          }
+          prevDigit = digit;
+        }
+        hasDouble = hasDouble || currentStreak == 2;
+
+        totalPasswords += (hasDouble && isIncreasing ? 1 : 0);
+      }
+
+      return totalPasswords;
+    }
+  }
+}
